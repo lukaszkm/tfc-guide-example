b9c97ce8ca8524f4684b3059613241a6e1579400
LukaszKM <mruk.lukasz@gmail.com>
2021-01-31 16:51:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,27 +4,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "random" {
-  version = "2.2"
-}
-
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-  range_key      = "UserName"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-
-  attribute {
-    name = "UserName"
-    type = "S"
-  }
+resource "aws_instance" "basic" {
+  ami           = "ami-0ee1a20d6b0c6a347"
+  instance_type = "t3.nano"
 }
